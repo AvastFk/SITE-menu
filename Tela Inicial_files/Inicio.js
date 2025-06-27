06c8cd4981fdc201f29c4f1763aca17cbbcea49e
mAvastFk <miguelpecanha2020@gmail.com>
2025-06-27 09:01:48 -0300

    Alteração do IP

--- Tela Inicial_files/Inicio.js
+++ Tela Inicial_files/Inicio.js
@@ -1,4 +1,4 @@
 // Quando clicar no botão ENTRAR, vai para o site principal
 document.getElementById("entrarBtn").addEventListener("click", function () {
-  window.location.href = "Reparabimt.html"; // troque "site.html" pelo seu site real
+  window.location.href = "http://127.0.0.1:5500/SITE%20PROPRIO/SITE%20menu/Reparabit.html"; // troque "site.html" pelo seu site real
 });
